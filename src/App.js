ee4af228c0c445029c002e9befe7d53873a57a2e
mollikacomputer <mollikacomputer3@gmail.com>
2022-06-26 21:07:49 +0600

    post data for client to server

--- src/App.js
+++ src/App.js
@@ -11,9 +11,35 @@ function App() {
     .then(data => setUsers(data));
   },[]);
 
+  const handleAddUser = event =>{
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    console.log(name, email);
+    const user = {name, email};
+
+    // send data to server
+    fetch('http://localhost:5000/user', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+
+  }
   return (
     <div className="App">
       <h2> Total users : {users.length} </h2>
+      <form onSubmit={handleAddUser} >
+        <input type="text" name='name' placeholder='name' />
+        <input type="email" name='email' placeholder='email' />
+        <input type="submit" value="add user" />
+      </form>
       <ul>
         {
           users.map(user => <li key={user.id} > user Name:{user.name},  user age: {user.age}, user phone : {user.phone}  </li>)
