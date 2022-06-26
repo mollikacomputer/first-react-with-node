dc798a6cbea7594eecf38ca25c4af4d2d5fb816f
mollikacomputer <mollikacomputer3@gmail.com>
2022-06-26 06:26:48 +0600

    initialized first react with node client side user connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Total users : {users.length} </h2>
+      {/* <ul>
+        users.map( user => <li key={user.id} > User Name : {user.name} </li> )
+      </ul> */}
     </div>
   );
 }
