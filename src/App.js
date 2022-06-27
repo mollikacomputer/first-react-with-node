eee5c2703141a817b3ad9853b1b953e7157e3173
mollikacomputer <mollikacomputer3@gmail.com>
2022-06-27 07:28:41 +0600

    user input field show UI successfully and input field clear

--- src/App.js
+++ src/App.js
@@ -15,34 +15,41 @@ function App() {
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
-    console.log(name, email);
+    // console.log(name, email);
     const user = {name, email};
 
     // send data to server
-    fetch('http://localhost:5000/user', {
+    fetch('http://localhost:5000/user', 
+    {
     method: 'POST', // or 'PUT'
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(user),
-  })
+  }
+  )
   .then(response => response.json())
   .then(data => {
+    const newUsers = [...users, data];
+    setUsers(newUsers);
     console.log('Success:', data);
   })
-
+  // for input field blank after submit
+  event.target.name.value = "";
+  event.target.email.value = "";
   }
+
   return (
     <div className="App">
       <h2> Total users : {users.length} </h2>
-      <form onSubmit={handleAddUser} >
+      <form name='user-form' onSubmit={handleAddUser} >
         <input type="text" name='name' placeholder='name' />
         <input type="email" name='email' placeholder='email' />
         <input type="submit" value="add user" />
       </form>
       <ul>
         {
-          users.map(user => <li key={user.id} > user Name:{user.name},  user age: {user.age}, user phone : {user.phone}  </li>)
+          users.map(user => <li key={user.id} > {user.id}. user Name:{user.name},  email : {user.email}  </li>)
         }
       </ul>
     </div>
